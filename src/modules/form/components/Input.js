d76da128db55f530464423a63bfa07c926e10361
Fredrik Olovsson <fogithub@gmail.com>
2017-11-08 12:56:01 +0100

    Update Input component
    To support label and helpText and only add space for icon if specified.
    Also add propTypes.

--- src/modules/form/components/Input.js
+++ src/modules/form/components/Input.js
@@ -1,21 +1,61 @@
-/* eslint-disable react/prop-types */
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Form, Input } from 'semantic-ui-react'
 import { FormFieldError } from '../../error/components'
 
+const propTypes = {
+  errorScope: PropTypes.string,
+  helpText: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  icon: PropTypes.string,
+  iconPosition: PropTypes.string,
+  input: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  meta: PropTypes.shape({
+    error: PropTypes.object,
+    touched: PropTypes.bool.isRequired,
+  }).isRequired,
+  module: PropTypes.string.isRequired,
+  required: PropTypes.bool,
+  type: PropTypes.string.isRequired,
+}
+const defaultProps = {
+  errorScope: undefined,
+  helpText: undefined,
+  icon: undefined,
+  iconPosition: 'left',
+  label: undefined,
+  required: false,
+}
+
 const Field = ({
+  errorScope,
+  label,
   icon,
-  module,
+  iconPosition,
   input,
   meta: { touched, error },
+  module,
+  required,
+  helpText,
   type,
-  errorScope,
   ...rest
 }) => {
   const displayError = touched && !!error
   return (
-    <Form.Field error={displayError} style={{ position: 'relative' }}>
-      <Input icon={icon} iconPosition="left" type={type} {...input} {...rest} />
+    <Form.Field
+      error={displayError}
+      required={required}
+      style={{ position: 'relative' }}
+    >
+      {label && <label htmlFor={input.name}>{label}</label>}
+      {helpText && <p>{helpText}</p>}
+      <Input
+        icon={icon}
+        iconPosition={icon && iconPosition}
+        type={type}
+        {...input}
+        {...rest}
+      />
       {displayError && (
         <FormFieldError
           error={error}
@@ -26,5 +66,8 @@ const Field = ({
     </Form.Field>
   )
 }
+
+Field.propTypes = propTypes
+Field.defaultProps = defaultProps
 
 export default Field
