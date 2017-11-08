6597b2993b04906c05ebe28dc870fb49a1dda00e
Fredrik Olovsson <fogithub@gmail.com>
2017-11-08 12:58:20 +0100

    Add placeholder endpoint for REGISTER_MAMMAL

--- src/apps/collectionsUi/config/index.js
+++ src/apps/collectionsUi/config/index.js
@@ -8,6 +8,7 @@ const config = {
     endpointKeyPathnameMap: {
       GET_USER: '/auth/realms/dina/protocol/openid-connect/userinfo',
       LOG_IN: '/auth/realms/dina/protocol/openid-connect/token',
+      REGISTER_MAMMAL: 'TBD',
     },
   },
   devToolsExtension:
