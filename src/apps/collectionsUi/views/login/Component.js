9b81e02cf8313e92ec93d2381edc08ac1b3d5a65
Fredrik Olovsson <fogithub@gmail.com>
2017-11-08 12:56:23 +0100

    Remove placeholder message from Login component

--- src/apps/collectionsUi/views/login/Component.js
+++ src/apps/collectionsUi/views/login/Component.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Image, Message } from 'semantic-ui-react'
+import { Grid, Image } from 'semantic-ui-react'
 import { LoginForm } from 'modules/user/components'
 import PageTemplate from 'modules/commonUi/components/PageTemplate'
 import logo from './logo.png'
@@ -11,7 +11,6 @@ const Login = () => (
         <Grid.Column style={{ marginTop: 100, maxWidth: 450 }}>
           <Image centered src={logo} style={{ height: 70, marginBottom: 20 }} />
           <LoginForm />
-          <Message>Common Login!</Message>
         </Grid.Column>
       </Grid>
     </div>
