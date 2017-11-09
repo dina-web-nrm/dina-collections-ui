7d7184f2ec29bda25ac9fc12f5b4ad84fd9df80d
antonoberg <anton.oberg@mobiento.com>
2017-11-09 14:35:33 +0100

    Adapt docker approach to dina-collections-docker

--- run.sh
+++ run.sh
@@ -2,15 +2,7 @@
 set -e
 set -x
 
-export NODE_ENV="${NODE_ENV:-development}"
+mkdir -p $ROOT/logs/nginx
+nginx -g 'daemon off;' -c $ROOT/src/nginx.conf
+nginx -c $ROOT/src/nginx.conf
 
-if [ $NODE_ENV == "development" ]; then
-  # this runs webpack-dev-server with hot reloading
-  npm start
-else
-  # build the app and serve it via nginx
-  npm run build
-  mkdir -p $ROOT/logs/nginx
-  nginx -g 'daemon off;' -c $ROOT/src/nginx.conf
-  nginx -c $ROOT/src/nginx.conf
-fi
