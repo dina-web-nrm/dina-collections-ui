ee89be59633d9915298da635e9bdd8c28a7a4de2
Fredrik Olovsson <fogithub@gmail.com>
2017-11-21 13:53:45 +0100

    Refactor NavigationSidebar for dynamic routes

--- src/coreModules/commonUi/components/NavigationSidebar.js
+++ src/coreModules/commonUi/components/NavigationSidebar.js
@@ -4,24 +4,16 @@ import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { NavLink, withRouter } from 'react-router-dom'
 import { Sidebar, Icon, Menu } from 'semantic-ui-react'
-import userLocalSelectors from 'coreModules/user/globalSelectors'
 import logoutActionCreator from 'coreModules/user/actionCreators/logout'
 import { createModuleTranslate } from 'coreModules/i18n/components'
 
 const ModuleTranslate = createModuleTranslate('commonUi')
-
-const mapStateToProps = state => {
-  return {
-    isLoggedIn: userLocalSelectors.getUserLoggedIn(state),
-  }
-}
 
 const mapDispatchToProps = {
   logout: logoutActionCreator,
 }
 
 const propTypes = {
-  isLoggedIn: PropTypes.bool.isRequired,
   logout: PropTypes.func.isRequired,
   navItems: PropTypes.arrayOf(
     PropTypes.shape({
@@ -30,8 +22,6 @@ const propTypes = {
       name: PropTypes.string.isRequired,
       path: PropTypes.string.isRequired,
       push: PropTypes.bool,
-      requireLoggedInUser: PropTypes.bool,
-      requireLoggedOutUser: PropTypes.bool,
     })
   ),
 }
@@ -40,8 +30,8 @@ const defaultProps = {
   navItems: [],
 }
 
-const NavigationSidebar = ({ isLoggedIn, logout, navItems }) => {
-  return !isLoggedIn ? null : (
+const NavigationSidebar = ({ logout, navItems }) => {
+  return (
     <Sidebar
       animation="overlay"
       as={Menu}
@@ -52,41 +42,20 @@ const NavigationSidebar = ({ isLoggedIn, logout, navItems }) => {
       visible
       width="thin"
     >
-      {navItems
-        .map(
-          ({
-            exact,
-            icon,
-            name,
-            path,
-            requireLoggedInUser,
-            requireLoggedOutUser,
-            push,
-          }) => {
-            if (requireLoggedInUser && !isLoggedIn) {
-              return null
-            }
-
-            if (requireLoggedOutUser && isLoggedIn) {
-              return null
-            }
-
-            return (
-              <NavLink
-                activeClassName="active"
-                className={push ? 'item push bottom' : 'item'}
-                exact={exact}
-                key={path}
-                to={path}
-              >
-                {icon && <Icon name={icon} size="large" />}
-                <ModuleTranslate capitalize textKey={`routes.${name}`} />
-              </NavLink>
-            )
-          }
+      {navItems.map(({ exact, icon, name, path, push }) => {
+        return (
+          <NavLink
+            activeClassName="active"
+            className={push ? 'item push bottom' : 'item'}
+            exact={exact}
+            key={path}
+            to={path}
+          >
+            {icon && <Icon name={icon} size="large" />}
+            <ModuleTranslate capitalize textKey={`routes.${name}`} />
+          </NavLink>
         )
-        .filter(component => !!component)}
-
+      })}
       <Menu.Item
         onClick={event => {
           event.preventDefault()
@@ -103,7 +72,6 @@ const NavigationSidebar = ({ isLoggedIn, logout, navItems }) => {
 NavigationSidebar.propTypes = propTypes
 NavigationSidebar.defaultProps = defaultProps
 
-export default compose(
-  withRouter,
-  connect(mapStateToProps, mapDispatchToProps)
-)(NavigationSidebar)
+export default compose(withRouter, connect(undefined, mapDispatchToProps))(
+  NavigationSidebar
+)
