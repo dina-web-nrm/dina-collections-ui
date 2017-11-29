33c7cf42f4c294d8fd3541a47a4d256c91a86634
Fredrik Olovsson <fogithub@gmail.com>
2017-11-29 12:14:04 +0100

    Move response validation to createResponse

--- src/coreModules/api/apiClient/createResponse/index.js
+++ src/coreModules/api/apiClient/createResponse/index.js
@@ -1,12 +1,29 @@
-import validateResponse from '../validateResponse'
+import chainPromises from 'utilities/chainPromises'
 
-export default function createResponse({ endpointConfig, responseData }) {
-  const { responseParser } = endpointConfig
+const extractMethodsFromConfigs = (configs, key) => {
+  return configs.map(config => config[key])
+}
 
-  return Promise.resolve(
-    responseParser ? responseParser(responseData) : responseData
-  ).then(response => {
-    return validateResponse({ endpointConfig, response }).then(() => {
+export default function createResponse({
+  apiConfig,
+  endpointConfig,
+  methodConfig,
+  responseData,
+}) {
+  const configs = [apiConfig, endpointConfig, methodConfig]
+
+  return Promise.all([
+    chainPromises(
+      extractMethodsFromConfigs(configs, 'responseValidation'),
+      responseData
+    ),
+  ]).then(() => {
+    return Promise.all([
+      chainPromises(
+        extractMethodsFromConfigs(configs, 'responseParser'),
+        responseData || {}
+      ),
+    ]).then(([response]) => {
       return response
     })
   })
--- src/coreModules/api/apiClient/validateResponse/index.js
+++
@@ -1,7 +0,0 @@
-export default function validateResponse({ endpointConfig, response }) {
-  const { responseValidation } = endpointConfig
-
-  return Promise.resolve(
-    responseValidation ? responseValidation(response) : response
-  )
-}
