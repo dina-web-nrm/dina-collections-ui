8ff741bafa83a114feb31cee6c8fd412c093e4de
Fredrik Olovsson <fogithub@gmail.com>
2017-11-29 11:39:10 +0100

    Add bodyValidation in endpointSpecFactory

--- src/coreModules/api/endpointSpecFactory/index.js
+++ src/coreModules/api/endpointSpecFactory/index.js
@@ -1,5 +1,7 @@
 import openApiSpec from 'dina-schema/build/openApi.json'
 
+import { createSystemModelSchemaValidator } from 'coreModules/error/utilities'
+
 const buildOperationIdPathnameMap = () => {
   const map = {}
   Object.keys(openApiSpec.paths).forEach(pathname => {
@@ -20,13 +22,47 @@ const buildOperationIdPathnameMap = () => {
 
 const map = buildOperationIdPathnameMap()
 
+const getModelNameFromParameter = ({ schema }) => {
+  if (!schema) {
+    return null
+  }
+
+  const segments = schema.$ref.split('/')
+
+  return segments[segments.length - 1]
+}
+
+const getBodyValidator = ({ methodSpecification }) => {
+  const { parameters } = methodSpecification
+
+  if (!parameters) {
+    return null
+  }
+
+  const bodyParameter = parameters.find(parameterSpecification => {
+    return parameterSpecification.in === 'body'
+  })
+
+  if (bodyParameter) {
+    const modelName = getModelNameFromParameter(bodyParameter)
+    return createSystemModelSchemaValidator({ model: modelName })
+  }
+
+  return null
+}
+
 export const buildEndpointSpec = ({ operationId, ...rest }) => {
   if (!map[operationId]) {
     throw new Error(`Operation id: ${operationId} unknown`)
   }
-  const { pathname } = map[operationId]
+  const { methodName, methodSpecification, pathname } = map[operationId]
 
   return {
+    bodyValidation: getBodyValidator({
+      methodName,
+      methodSpecification,
+      pathname,
+    }),
     key: operationId,
     pathname,
     ...rest,
