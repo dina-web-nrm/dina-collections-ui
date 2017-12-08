a701781d6007c0aeb72da0fb310d7a21d6331895
Fredrik Olovsson <fogithub@gmail.com>
2017-12-08 15:24:27 +0100

    Fix interpolateUrl to match API-spec pattern

--- src/coreModules/api/apiClient/fetch/interpolateUrl.js
+++ src/coreModules/api/apiClient/fetch/interpolateUrl.js
@@ -1,11 +1,11 @@
-const pathParametersRegex = /\${([a-zA-Z0-9]+)}/g
+const pathParametersRegex = /\{([a-zA-Z0-9]+)}/g
 
 module.exports = function interpolateUrl(url, pathParams) {
   let resultURL = url
   const requiredPathParameters = resultURL.match(pathParametersRegex) || []
 
   requiredPathParameters.forEach(p => {
-    const cleanParameter = p.replace(/[${}]/g, '') // yes
+    const cleanParameter = p.replace(/[{}]/g, '') // yes
     if (pathParams[cleanParameter] !== undefined) {
       resultURL = resultURL.replace(`${p}`, pathParams[cleanParameter])
     } else {
