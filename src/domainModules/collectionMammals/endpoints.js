a9d942f2e33a69b6607626a55305084b2e20939f
antonoberg <oberg.anton@gmail.com>
2017-12-11 10:55:26 +0100

    Use automatic generated validateResponse in col...Mammals

--- src/domainModules/collectionMammals/endpoints.js
+++ src/domainModules/collectionMammals/endpoints.js
@@ -1,30 +1,25 @@
 import { buildEndpointSpec } from 'coreModules/api/endpointSpecFactory'
-import { createSystemSchemaValidator } from 'utilities/error'
 import { createLookupMammalsResponse, getIndividualGroup } from './mockData'
 
-import { lookupMammalsResponse, registerMammalResponse } from './schemas'
-
 export const GET_INDIVIDUAL_GROUP_BY_CATALOG_NUMBER = buildEndpointSpec({
-  mapResponse: json => {
-    return json.data[0] // should only be one result, which holds for mammals
+  mapResponse: result => {
+    return result.data[0] // should only be one result, which holds for mammals
   },
+
   mock: ({ request: { queryParams } }) => {
     return { data: [getIndividualGroup(queryParams)] }
   },
   operationId: 'getIndividualGroups',
-  validateResponse: createSystemSchemaValidator(lookupMammalsResponse),
 })
 
 export const LOOKUP_MAMMALS = buildEndpointSpec({
   mock: createLookupMammalsResponse,
   operationId: 'getIndividualGroups',
-  validateResponse: createSystemSchemaValidator(lookupMammalsResponse),
 })
 
 export const REGISTER_MAMMAL = buildEndpointSpec({
   mock: ({ request }) => request.body,
   operationId: 'createIndividualGroup',
-  validateResponse: createSystemSchemaValidator(registerMammalResponse),
 })
 
 export const UPDATE_INDIVIDUAL_GROUP = buildEndpointSpec({
