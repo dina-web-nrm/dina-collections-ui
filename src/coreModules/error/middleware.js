1aedbaa3f841e3f6bccdf1646a3ab64311aecb01
antonoberg <oberg.anton@gmail.com>
2017-12-12 11:50:36 +0100

    Log instead of throw error in error middleware

--- src/coreModules/error/middleware.js
+++ src/coreModules/error/middleware.js
@@ -10,7 +10,7 @@ export default function errorMiddleware({ debug = true } = {}) {
       if (isKnownError(action.payload)) {
         console.log(`Error in action ${action.type}:`, action.payload) // eslint-disable-line no-console
       } else if (process.env.NODE_ENV === 'development') {
-        throw action.payload
+        console.log(action.payload) // eslint-disable-line no-console
       }
     }
     return result
