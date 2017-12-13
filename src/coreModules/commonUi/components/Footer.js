9581e4ddb26b3da7e9406ee17fd7cc2e439bbe49
Fredrik Olovsson <fogithub@gmail.com>
2017-12-13 16:24:55 +0100

    Update content in Footer

--- src/coreModules/commonUi/components/Footer.js
+++ src/coreModules/commonUi/components/Footer.js
@@ -11,30 +11,17 @@ export default () => {
             <Grid.Column width={3}>
               <Header as="h4" content="About" inverted />
               <List inverted link size={size}>
-                <List.Item as="a">Dina project page</List.Item>
-                <List.Item as="a">
-                  <List.Content>Github</List.Content>
+                <List.Item>
+                  <a href="https://www.dina-project.net/wiki/Welcome_to_the_DINA_project!">
+                    DINA-Project
+                  </a>
                 </List.Item>
-                <List.Item as="a">Contact Us</List.Item>
+                <List.Item>
+                  <List.Content>
+                    <a href="https://github.com/DINA-Web">DINA-Web on Github</a>
+                  </List.Content>
+                </List.Item>
               </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header as="h4" content="Services" inverted />
-              <List inverted link>
-                <List.Item as="a">Banana Pre-Order</List.Item>
-                <List.Item as="a">DNA FAQ</List.Item>
-                <List.Item as="a">How To Access</List.Item>
-                <List.Item as="a">Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as="h4" inverted>
-                Footer Header
-              </Header>
-              <p>
-                Extra space for a call to action inside the footer that could
-                help re-engage users.
-              </p>
             </Grid.Column>
           </Grid.Row>
         </Grid>
