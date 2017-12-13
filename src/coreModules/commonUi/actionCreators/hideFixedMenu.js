a9107c8d653c70e41f7d04851b5da2e00aa7fb05
Fredrik Olovsson <fogithub@gmail.com>
2017-12-13 16:24:29 +0100

    Add FixedMenu component and state management

---
+++ src/coreModules/commonUi/actionCreators/hideFixedMenu.js
@@ -0,0 +1,7 @@
+import { COMMON_UI_HIDE_FIXED_MENU } from '../actionTypes'
+
+export default function hideFixedMenu() {
+  return {
+    type: COMMON_UI_HIDE_FIXED_MENU,
+  }
+}
--- src/coreModules/commonUi/selectors.js
+++ src/coreModules/commonUi/selectors.js
@@ -1,8 +1,10 @@
 import { createGetter } from 'utilities/stateHelper'
 
 const sidebarOpenGetter = createGetter(['sidebar', 'isOpen'])
+const fixedMenuVisibleGetter = createGetter(['fixedMenu', 'isVisible'])
 
 export const getLocalState = state => state.commonUi
 
+export const getFixedMenuIsVisible = state => fixedMenuVisibleGetter(state)
 export const getSidebarIsOpen = state => sidebarOpenGetter(state)
 export const getSidebarIsClosed = state => !sidebarOpenGetter(state)
