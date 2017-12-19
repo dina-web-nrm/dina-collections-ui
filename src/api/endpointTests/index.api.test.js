8344f086ca51b48d5f008ee355b5f126b37b9a01
Fredrik Olovsson <fogithub@gmail.com>
2017-12-19 18:01:02 +0100

    Remove old API test files

--- src/api/endpointTests/index.api.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable global-require */
-
-if (process.env.API_TEST) {
-  require('./collections/postIndividualGroups')
-} else {
-  it('api test not running without flag API_TEST', () => {
-    expect.assertions(0)
-  })
-}
