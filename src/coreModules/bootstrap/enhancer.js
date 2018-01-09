44fc60c66e3b03bedc42a9ab94af301ef0418f14
antonoberg <oberg.anton@gmail.com>
2018-01-09 17:08:17 +0100

    Refactor bootstrap enhancer
    Move functionality to separate functions instead of keeping it in the
    enhancer

--- src/coreModules/bootstrap/enhancer.js
+++ src/coreModules/bootstrap/enhancer.js
@@ -2,21 +2,20 @@ import { compose } from 'redux'
 import createLog from 'utilities/log'
 import Dependor from 'utilities/Dependor'
 
-import createSerializedModuleMap from './utilities/createSerializedModuleMap'
-import registerModulesActionCreator from './actionCreators/registerModules'
-import startListeners from './utilities/startListeners'
-import unregisterModulesActionCreator from './actionCreators/unregisterModules'
-import updateModuleState from './utilities/updateModuleState'
+import createRegisterModules from './utilities/createRegisterModules'
+import createUnregisterModules from './utilities/createUnregisterModules'
+import createEnhancerStateManager from './utilities/createEnhancerStateManager'
+import createAsyncRegisterModules from './utilities/createAsyncRegisterModules'
+import createAsyncUnregisterModules from './utilities/createAsyncUnregisterModules'
 
 const log = createLog('modules:bootstrap:enhancer')
 
 export const dep = new Dependor(
   {
-    createSerializedModuleMap,
-    registerModulesActionCreator,
-    startListeners,
-    unregisterModulesActionCreator,
-    updateModuleState,
+    createAsyncRegisterModules,
+    createAsyncUnregisterModules,
+    createRegisterModules,
+    createUnregisterModules,
   },
   'modules:bootstrap:enhancer'
 )
@@ -32,90 +31,54 @@ export default function updateStoreEnhancer({ config, moduleOrder = [] }) {
   return createStore => (...params) => {
     log.info('Create store')
     const store = createStore(...params)
+    const { dispatch } = store
 
-    let { dispatch } = store
-    let chain = []
+    const {
+      getDispatch,
+      getModuleState,
+      setDispatch,
+      setModuleState,
+    } = createEnhancerStateManager({
+      dispatch,
+    })
 
     const middlewareApi = {
-      dispatch: (...args) => dispatch(...args),
+      dispatch: (...args) => getDispatch()(...args),
       getState: store.getState,
     }
 
     const updateDispatch = middlewares => {
-      chain = middlewares.map(middleware => middleware(middlewareApi))
-      dispatch = compose(...chain)(store.dispatch)
+      const chain = middlewares.map(middleware => middleware(middlewareApi))
+      setDispatch(compose(...chain)(store.dispatch))
     }
 
     const dynamicDispatch = (...args) => {
-      return dispatch(...args)
+      return getDispatch()(...args)
     }
 
-    let moduleState = {}
+    const registerModules = dep.createRegisterModules({
+      availableModules,
+      config,
+      dynamicDispatch,
+      getModuleState,
+      middlewareApi,
+      moduleOrder,
+      setModuleState,
+      store,
+      updateDispatch,
+    })
 
-    const registerModules = modules => {
-      log.info(
-        'Register modules start:',
-        modules.map(({ name }) => name).join(', ')
-      )
-      moduleState = dep.updateModuleState({
-        ...moduleState,
-        availableModules,
-        config,
-        middlewareApi,
-        moduleOrder,
-        newModules: modules,
-      })
-      log.debug('Module state updated')
-      const { middlewareArray, reducer } = moduleState
-
-      updateDispatch(middlewareArray)
-      log.debug('Dispatch updated')
-      store.replaceReducer(reducer)
-      log.debug('Reducer replaced')
-      // leave this to translation module instead?
-      // might make sense to put in middleware
-      // add pre and post / success
-      dynamicDispatch(
-        dep.registerModulesActionCreator({
-          config,
-          modules: dep.createSerializedModuleMap(modules),
-        })
-      )
-      dep.startListeners({
-        listenerMap: moduleState.listenerMap,
-        middlewareApi,
-      })
-      log.debug('Listeners started')
-      log.info('Register modules done')
-    }
-
-    const unregisterModules = modules => {
-      log.info(
-        'Unregister modules start:',
-        modules.map(({ name }) => name).join(', ')
-      )
-
-      moduleState = dep.updateModuleState({
-        ...moduleState,
-        availableModules,
-        config,
-        middlewareApi,
-        moduleOrder,
-        removeModules: modules,
-      })
-
-      const { middlewareArray, reducer } = moduleState
-
-      updateDispatch(middlewareArray)
-      store.replaceReducer(reducer)
-      dynamicDispatch(
-        dep.unregisterModulesActionCreator({
-          modules: dep.createSerializedModuleMap(modules),
-        })
-      )
-
-      log.info('Unregister modules done')
-    }
+    const unregisterModules = dep.createUnregisterModules({
+      availableModules,
+      config,
+      dynamicDispatch,
+      getModuleState,
+      middlewareApi,
+      moduleOrder,
+      setModuleState,
+      store,
+      updateDispatch,
+    })
 
     updateDispatch([])
 
