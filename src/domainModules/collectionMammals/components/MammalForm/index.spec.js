da08791b9c945f922174582b5e72a980502a8cd3
antonoberg <oberg.anton@gmail.com>
2018-01-12 14:35:40 +0100

    Add test for loading MammalForm with initial data

--- src/domainModules/collectionMammals/components/MammalForm/index.spec.js
+++ src/domainModules/collectionMammals/components/MammalForm/index.spec.js
@@ -61,4 +61,71 @@ describe('domainModules/collectionMammals/components/MammalForm', () => {
 
     expect(store.getState().form.mammalForm.submitFailed).toBe(undefined)
   })
+
+  it('Submit success when loaded with existing individual group', () => {
+    const handleFormSubmit = data => {
+      return Promise.resolve(data)
+    }
+
+    const individualGroup = {
+      attributes: {
+        featureObservations: [
+          {
+            featureObservationText: 'female',
+            featureObservationType: {
+              featureObservationTypeName: 'sex',
+              id: 1,
+            },
+          },
+        ],
+        identifications: [
+          {
+            identificationText: 'Water opossum',
+            identifiedByAgentText: 'Doe, J.',
+            identifiedTaxonNameStandardized: 'Chironectes minimus',
+          },
+        ],
+        occurrences: [
+          {
+            id: 1,
+            localityText: 'Hemsö',
+          },
+        ],
+        physicalUnits: [
+          {
+            catalogedUnit: {
+              catalogNumber: '444444',
+            },
+          },
+        ],
+      },
+      id: 2,
+      type: 'individualGroup',
+    }
+
+    const { rootComponent: mountedComponent, store } = setupTestComponent({
+      component: (
+        <MammalForm
+          handleFormSubmit={handleFormSubmit}
+          individualGroupAttributes={individualGroup.attributes}
+          individualGroupId={2}
+          transformOutputForUpdate
+        />
+      ),
+      fullExport: true,
+      mount: true,
+    })
+
+    const form = mountedComponent.find('form')
+    const catalogedNumberInput = form
+      .find({
+        name: 'physicalUnits[0].catalogedUnit.catalogNumber',
+      })
+      .hostNodes()
+
+    expect(catalogedNumberInput.props().value).toBe('444444')
+    form.simulate('submit')
+
+    expect(store.getState().form.mammalForm.submitFailed).toBe(undefined)
+  })
 })
