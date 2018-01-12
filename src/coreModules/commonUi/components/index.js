664697e2e37a99e693664f373e5f44a0674215f9
antonoberg <oberg.anton@gmail.com>
2018-01-12 10:57:55 +0100

    Remove not used Navbar component

--- src/coreModules/commonUi/components/index.js
+++ src/coreModules/commonUi/components/index.js
@@ -1,15 +1,7 @@
 import Footer from './Footer'
-import Navbar from './Navbar'
 import NavigationSidebar from './NavigationSidebar'
 import PageTemplate from './PageTemplate'
 import TranslatedLabel from './TranslatedLabel'
 import ViewWrap from './ViewWrap'
 
-export {
-  Footer,
-  Navbar,
-  NavigationSidebar,
-  PageTemplate,
-  TranslatedLabel,
-  ViewWrap,
-}
+export { Footer, NavigationSidebar, PageTemplate, TranslatedLabel, ViewWrap }
