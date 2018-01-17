fde0d8542dea5612dd0c638ccd05cf8797edf906
Fredrik Olovsson <fogithub@gmail.com>
2018-01-17 15:40:45 +0100

    Add localityInformationSearchQueries to dux

--- src/domainModules/collectionMammals/selectors.js
+++ src/domainModules/collectionMammals/selectors.js
@@ -9,6 +9,14 @@ export const getFeatureTypeNameSearchQueries = state => {
 export const getFeatureTypeNameSearchQuery = (state, inputName) => {
   const individualGroups = getFeatureTypeNameSearchQueries(state)
   return individualGroups[inputName]
+
+export const getLocalityInformationSearchQueries = state => {
+  return state.localityInformationSearchQueries
+}
+
+export const getLocalityInformationSearchQuery = (state, inputName) => {
+  const queries = getLocalityInformationSearchQueries(state)
+  return queries[inputName]
 }
 
 export const getLookupResult = state => {
