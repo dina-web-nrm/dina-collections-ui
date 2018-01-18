962b4ce18c7b05676bb55b77b1639232b31aedda
Fredrik Olovsson <fogithub@gmail.com>
2018-01-18 10:47:24 +0100

    Enable scope after index in fieldNamePathFactory

--- src/coreModules/form/utilities/fieldNamePathFactory.js
+++ src/coreModules/form/utilities/fieldNamePathFactory.js
@@ -1,5 +1,9 @@
-export default function fieldNamePathFactory(base) {
+export default function fieldNamePathFactory(base, scope = '') {
   return function buildFieldNamePath(name, index = 0) {
+    if (scope) {
+      return `${base}[${index}].${scope}.${name}`
+    }
+
     return `${base}[${index}].${name}`
   }
 }
