1dd07c90c381f7bd207cc3185f75158b48cd1a9c
Fredrik Olovsson <fogithub@gmail.com>
2018-01-18 12:35:30 +0100

    Add FormFieldHelpIcon component

---
+++ src/coreModules/notifications/components/FormFieldHelpIcon.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Icon } from 'semantic-ui-react'
+
+import createNotificationAC from '../actionCreators/createNotification'
+
+const mapDispatchToProps = { createNotification: createNotificationAC }
+
+const propTypes = {
+  createNotification: PropTypes.func.isRequired,
+  helpNotificationProps: PropTypes.shape({
+    description: PropTypes.node,
+    descriptionHeaderKey: PropTypes.string,
+    descriptionKey: PropTypes.string,
+    headerKey: PropTypes.string,
+    linkTextKey: PropTypes.string,
+    linkTo: PropTypes.string,
+    size: PropTypes.string,
+  }).isRequired,
+}
+
+const FormFieldHelpIcon = ({ createNotification, helpNotificationProps }) => {
+  return (
+    <Icon
+      color="blue"
+      link
+      name="help circle outline"
+      onClick={() =>
+        createNotification({
+          componentProps: helpNotificationProps,
+          type: 'HELP_TEXT',
+        })
+      }
+    />
+  )
+}
+
+FormFieldHelpIcon.propTypes = propTypes
+
+export default connect(undefined, mapDispatchToProps)(FormFieldHelpIcon)
